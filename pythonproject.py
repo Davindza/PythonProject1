94c3c40403e43b5bced5941c686c2f248bbe5dd9
David Obradovic <daviderror404@gmail.com>
2019-02-20 20:26:33 +0100

    pythonproject.py

---
+++ pythonproject.py
@@ -0,0 +1,19 @@
+mood = input("What is your mood today?")
+
+if mood == "happy":
+    print("It is great to see you happy")
+
+elif mood == "nervous":
+    print("Take a deep breath 3 times")
+
+elif mood == "sad":
+    print("Don't worry be happy")
+
+elif mood == "exicted":
+    print("Oh yees")
+
+elif mood =="relaxed":
+    print("Dude, just chill out and relax")
+
+else:
+    print( "I don't recognize this mood")
